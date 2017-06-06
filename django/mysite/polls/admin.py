0f07f05d30e4ba6d590be08798c23a5c728127b8
Maria Sumedre <maria.sumedre@3pillarglobal.com>
2017-06-06 16:01:10 +0300

    change admin according to tutorial

--- django/mysite/polls/admin.py
+++ django/mysite/polls/admin.py
@@ -7,6 +7,7 @@ from .models import Question, Choice, Poll
 
 class InlineChoiceAdmin(admin.TabularInline):
     model = Choice
+    extra = 2
 
 
 class ChoiceAdmin(admin.ModelAdmin):
@@ -19,9 +20,19 @@ class ChoiceAdmin(admin.ModelAdmin):
 
 
 class QuestionAdmin(admin.ModelAdmin):
+    ' specify how the admin page for a question looks like '
     inlines = [InlineChoiceAdmin, ]
+    list_display = ("question_text", "pub_date", "was_published_recently")
+    list_filter = ("pub_date",)
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+    search_fields = ("question_text","choice__choice_text")
+    list_per_page = 50
 
     class Meta:
+        ' model for admin is Question '
         model = Question
 
 
